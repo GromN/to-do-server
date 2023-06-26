8114f051f2da3a2df31177a8f5d7e47af9cf0837
Nataliia Hromenko <nataliia@capitalise.com>
2023-06-26 12:18:44 +0100

    use filtering

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/todoApp");
 app.use(express.json());
 
 const Todo = mongoose.model("Todo", {
+  _id: mongoose.Schema.Types.ObjectId,
   title: String,
   description: String,
   status: Number,
@@ -17,14 +18,10 @@ app.get("/todo", async (req, res) => {
   res.json(todoList);
 });
 
-app.get("/todo/:id", async (req, res) => {
+app.get("/todo/id/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-  const todoList = await Todo.find({
-    id: req.params.id,
-    // status: req.params.status,
-  });
-
-  res.json(todoList);
+  res.json(todo);
 });
 
 app.post("/todo", (req, res) => {
@@ -38,21 +35,36 @@ app.post("/todo", (req, res) => {
   res.json(req.body);
 });
 
+
 //connect to mongoDB
 //start mongo when the server starts (mongos orm - stackoverflow)
 //create a docker container and start a db inside of it
 
-app.delete("/todo", (req, res) => {
-  res.send("Hello World!");
+
+app.patch("/todo/:id", async (req, res) => {
+  const filter = { _id: req.params.id };
+  const update = req.body;
+
+  await Todo.findOneAndUpdate(filter, update);
+
 });
 
-app.patch("/todo", (req, res) => {
-  res.send("Hello World!");
+app.put("/todo/:id", async (req, res) => {
+  const filter = { _id: req.params.id };
+  const update = req.body;
+
+  await Todo.findOneAndUpdate(filter, update);
 });
 
-//use filtering in patch, put and delete
-//figure out hot to GET by id that is created by mongo (objectId -> string)
+app.delete("/todo/:id", async (req, res) => {
+  const filter = { _id: req.params.id };
+
+  await Todo.deleteOne(filter);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//use filtering in patch, put and delete
+//write GET request to get by id that is created by mongo (objectId -> string)
